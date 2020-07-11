aac9f02e70bf7c597453d88313b7f035e92a4b03
tws02 <59136053+tws02@users.noreply.github.com>
2020-07-11 07:18:25 -0700

    .

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const connectDB = require("./config/db");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+connectDB();
+
+// Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/auth", require("./routes/api/auth"));
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
